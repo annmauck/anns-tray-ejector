faa1c1e813e34da8548ece2fd48a48a4cad38cb8
Ann Mauck <steph.mauck@gmail.com>
2018-06-14 20:43:51 -0600

    Anns-Tray-Ejector
    A simple application that resides in the system tray and allows for ejecting one's optical drive tray by clicking or right clicking on the icon

---
+++ ate.py
@@ -0,0 +1,31 @@
+#Written by Ann Mauck
+#A simple application that resides in the system tray and allows for ejecting one's optical drive tray by clicking or right clicking on the icon
+
+import appindicator #appindicators are what Mint calls system tray icons
+import gtk 
+from subprocess import call #needed for bash commands
+
+ai = appindicator.Indicator('tray_ejector', 'icon.png', appindicator.CATEGORY_APPLICATION_STATUS)
+ai.set_status(appindicator.STATUS_ACTIVE)
+m = gtk.Menu()
+tt = gtk.MenuItem( 'Toggle Tray' )
+qi = gtk.MenuItem( 'Quit' )
+
+m.append(tt)
+m.append(qi)
+
+ai.set_menu(m)
+tt.show()
+qi.show()
+
+def eject(item):
+        call(['eject', '-T']) #calls bash's eject function with the toggle switch
+
+tt.connect('activate', eject)
+
+def quit(item):
+        gtk.main_quit()
+
+qi.connect('activate', quit)
+
+gtk.main()
